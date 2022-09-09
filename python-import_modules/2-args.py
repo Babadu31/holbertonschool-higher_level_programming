89955af0ea779fbcf54da000bb9788214e992e5a
Babadu31 <bastienmiss31@gmail.com>
2022-09-09 13:18:34 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,17 +4,18 @@ import sys
 
 def main(*argv):
     x = 0
-    l = len(sys.argv) - 1
-    if l == 1:
-        print("{:d} argument:".format(l))
-    elif l == 0:
-        print("{:d} arguments.".format(l))
+    y = len(sys.argv) - 1
+    if y == 1:
+        print("{:d} argument:".format(y))
+    elif y == 0:
+        print("{:d} arguments.".format(y))
     else:
-        print("{:d} arguments:".format(l))
+        print("{:d} arguments:".format(y))
     for args in sys.argv:
         if (x != 0):
             print("{}: {}".format(x, args))
         x += 1
+
 
 if __name__ == "__main__":
     main()
