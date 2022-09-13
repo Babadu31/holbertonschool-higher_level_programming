98ec2eb6c8f14efe7d03a15a8407629e64463fe1
Babadu31 <bastienmiss31@gmail.com>
2022-09-13 10:11:27 +0200

    2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    counts = {}
-    for i in my_list:
-        if i in counts.keys():
-            counts[i] += 1
-        else:
-            counts[i] = 1
-
-    result = [k for k, v in counts.items() if v == 1]
-    return sum(result)
+    new = set(my_list)
+    re = 0
+    for i in new:
+        re += i
+    return re
