240066d24787e45be81fe084729c3389f4fc3cd4
Babadu31 <bastienmiss31@gmail.com>
2022-09-19 11:49:13 +0200

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,6 +12,6 @@ example function class
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
-                raise ValueError("size must be >=0")
+                raise ValueError("size must be >= 0")
         else:
                 self.__size = size
