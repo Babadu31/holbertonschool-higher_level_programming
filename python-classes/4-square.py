f934dc41b70aa9f8f4d92e2ee169e919a1927b82
Babadu31 <bastienmiss31@gmail.com>
2022-09-19 13:27:12 +0200

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,7 +9,6 @@ class Square:
         attribute : dssd
             Returns: none
         """
-
         self.size = size
 
     def area(self):
@@ -19,25 +18,23 @@ class Square:
         """
         return (self.__size) ** 2
 
-
-@property
-def size(self):
-    """size of square
+    @property
+    def size(self):
+        """size of square
         attribute : dssd
         Returns: size of square
-    """
-    return self.__size
+        """
+        return self.__size
 
-
-@size.setter
-def size(self, value):
-    """set the size of square
+    @size.setter
+    def size(self, value):
+        """set the size of square
         attribute : dssd
         Returns: none
         """
-    if type(value) != int:
-        raise TypeError("size must be an integer")
-    elif value < 0:
-        raise ValueError("size must be >= 0")
-    else:
-        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
