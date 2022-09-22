f9ea950acf3194a12f9e409d176c5eaca841f8b7
Babadu31 <bastienmiss31@gmail.com>
2022-09-22 10:17:14 +0200

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Add Integer or Float
-"""
+"""In this module the functions errors() and add_integer() are defined"""
+
+
+def errors(value, name):
+    """Handles errors from add_integer()"""
+    if not isinstance(value, int) and not isinstance(value, float):
+        raise TypeError(f"{name} must be an integer")
 
 
 def add_integer(a, b=98):
-    """
-    ADD Two integer a and b
-    """
-    if type(a) not in [int, float]:
-        raise TypeError('a must be an integer')
-    if type(b) not in [int, float]:
-        raise TypeError('b must be an integer')
-    if (a + b) == float('inf') or (a + b) == -float('inf'):
-        return b
+    """Returns the sum between a and b"""
+    errors(a, "a")
+    errors(b, "b")
     return int(a) + int(b)
