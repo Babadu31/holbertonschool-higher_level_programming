516319b19937068b5f9601382e008a511f4b0997
Babadu31 <bastienmiss31@gmail.com>
2022-10-03 03:51:22 -0700

    2

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,9 +9,9 @@ class Base:
     """
     Base class
     """
-    __nb_objects = 0
+    __nb_objects = 0   # private class attribute
 
-    def __init__(self, id=None):
+    def __init__(self, id=None):  # class constructor
         """
         Init
         Attributes:
@@ -19,6 +19,14 @@ class Base:
         """
         if id is not None:
             self.id = id
+            """
+            if id is not None, assign the public instance
+            attribute id with this argument value
+            """
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+        """"
+        otherwise, increment __nb_objects and assign the new value
+        to the public instance attribute id
+        """
