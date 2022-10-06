c48ae9685670f6fdacae2fa5020c17feda7d73b2
Babadu31 <bastienmiss31@gmail.com>
2022-10-06 03:59:03 -0700

    test2

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,7 @@ tests for class
 import unittest
 import sys
 from models.base import Base
+import json
 
 class TestBaseClass(unittest.TestCase):
     """Class testing the Base class
@@ -26,38 +27,19 @@ class TestBaseClass(unittest.TestCase):
         b98 = Base(98)
         self.assertEqual(b98.id, 98)
 
-    def _nb_private(self):
-        """Testing nb_objects as a private instance attribute"""
-        b = Base(3)
-        with self.assertRaises(AttributeError):
-            print(b.nb_objects)
-        with self.assertRaises(AttributeError):
-            print(b.__nb_objects)
-
-    def _to_json_string(self):
-        """Testing regular to json string"""
-        Base._Base__nb_objects = 0
-        d1 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
-        d2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
-        json_s = Base.to_json_string([d1, d2])
-        self.assertTrue(type(json_s) is str)
-        d = json.loads(json_s)
-        self.assertEqual(d, [d1, d2])
-
-    def _empty_to_json_string(self):
-        """Test for passing empty list"""
-        json_s = Base.to_json_string([])
-        self.assertTrue(type(json_s) is str)
-        self.assertEqual(json_s, "[]")
-
-    def _None_to_json_String(self):
-        """testting None to a json"""
+    def test_None_to_json_String(self):
         json_s = Base.to_json_string(None)
         self.assertTrue(type(json_s) is str)
         self.assertEqual(json_s, "[]")
 
-    def _from_json_string(self):
-        """Tests normal from_json_string"""
+    def test_empty_to_json_string(self):
+        """Test for passing empty list/ None"""
+        json_s = Base.to_json_string([])
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_from_json_string(self):
+        """Tests regular from_json_string"""
         json_str = '[{"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}, \
 {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
         json_l = Base.from_json_string(json_str)
@@ -69,4 +51,3 @@ class TestBaseClass(unittest.TestCase):
                          {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8})
         self.assertEqual(json_l[1],
                          {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
-
