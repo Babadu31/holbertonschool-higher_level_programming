67a74d227432829108114e05e2b3c7d38536a7c1
Babadu31 <bastienmiss31@gmail.com>
2022-10-06 03:38:59 -0700

    17-0

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -10,18 +10,20 @@ from models.base import Base
 class TestBaseClass(unittest.TestCase):
     """Class testing the Base class
     """
-    
+
     def test_no_id(self):
         """Tests id as None"""
         b = Base()
         self.assertEqual(b.id, 1)
+
+    def test_no_id_after_set(self):
+        """Tests id as None after not None"""
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
 
     def test_id_set(self):
         """Tests id as not None"""
         b98 = Base(98)
         self.assertEqual(b98.id, 98)
 
-    def test_no_id_after_set(self):
-        """Tests id as None after not None"""
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
+
