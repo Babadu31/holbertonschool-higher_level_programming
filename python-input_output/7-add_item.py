97c21924fa6ca688a2f1585edef04f5c9d623720
Babadu31 <bastienmiss31@gmail.com>
2022-10-07 02:01:34 -0700

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that adds all arguments to a Python list, and then save them to a file
+Write a script that adds all arguments
+to a Python list, and then save them to a file
 """
 import sys
 
