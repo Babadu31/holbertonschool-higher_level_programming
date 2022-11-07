62984a6e0566e75ca03fc18c9e024de1ed7856be
Babadu31 <bastienmiss31@gmail.com>
2022-11-07 05:51:00 -0800

    07/11/22-14:50-7-3

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,10 +6,11 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table cities already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities 
+(
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id);
-)
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+);
