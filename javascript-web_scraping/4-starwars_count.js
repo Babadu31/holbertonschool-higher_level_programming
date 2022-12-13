0e8fcb4ff8398f07e5d1ddf70f4edb5ccf73f406
Babadu31 <bastienmiss31@gmail.com>
2022-12-13 02:02:49 -0800

    11:02_13/12/22_4-1

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, function (error, response, body) {
+  let count = 0;
+  if (error) throw (error);
+  const data = JSON.parse(body);
+  for (let x = 0; data.results[x] !== undefined; x++) {
+    if (data.results[x].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      count++;
+    }
+  }
+  console.log(count);
+});
