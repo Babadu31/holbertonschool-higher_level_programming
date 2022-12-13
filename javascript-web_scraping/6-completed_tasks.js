c6e958a9819fdf353307b8051b0d72fa1a640d88
Babadu31 <bastienmiss31@gmail.com>
2022-12-13 04:02:19 -0800

    13:02_13/12/22_6-1

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, res, body) {
+  if (error) throw (error);
+  const data = JSON.parse(body);
+  const result = {};
+
+  for (const task of data) {
+    if (task.completed) {
+      const userId = String(task.userId);
+      if (Object.keys(result).includes(userId)) {
+        result[userId] = result[userId] + 1;
+      } else {
+        result[userId] = 1;
+      }
+    }
+  }
+  console.log(result);
+});
