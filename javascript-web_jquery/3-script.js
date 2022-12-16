2784a3b5f0a9b3173ad6e38a8ef02cc7875cdb76
Babadu31 <bastienmiss31@gmail.com>
2022-12-16 04:30:17 -0800

    13:29_16/12/22_4-1

--- javascript-web_jquery/3-script.js
+++ javascript-web_jquery/3-script.js
@@ -2,3 +2,12 @@ const $ = window.$;
 $('DIV#red_header').click(function () {
   $('header').addClass('red');
 });
+
+/*
+const $ = window.$;
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').addClass('red');
+  });
+});
+*/
