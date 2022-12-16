ad02ffd4da3439ad8ae9733586c42a20c279a828
Babadu31 <bastienmiss31@gmail.com>
2022-12-16 06:04:36 -0800

    15:04_16/12/22_11-1

--- javascript-web_jquery/100-script.js
+++ javascript-web_jquery/100-script.js
@@ -1,3 +1,11 @@
 window.onload = function () {
   document.querySelector('header').style.color = '#FF0000';
 };
+
+/*
+(function () {
+  document.addEventListener('DOMcontentLoaded', function () {
+    document.querySelector('header').style.color = '#FF0000';
+  }, false);
+})();
+*/
