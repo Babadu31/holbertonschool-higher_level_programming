878b7731580a0065e9ed330f6ee6aaf8f748ff8c
Babadu31 <bastienmiss31@gmail.com>
2022-12-16 06:43:35 -0800

    15:43_16/12/22_12-2

--- javascript-web_jquery/102-script.js
+++ javascript-web_jquery/102-script.js
@@ -25,3 +25,22 @@ $(document).ready(() => {
 })
 ;
 */
+
+/*
+const $ = window.$;
+$(document).ready(function () {
+  $('#btn_translate').click(function () {
+    const languageCode = $('#language_code').val();
+
+    $.ajax({
+      url: 'https://www.fourtonfish.com/hellosalut/hello/',
+      data: {
+        lang: languageCode
+      },
+      success: function (data) {
+        $('#hello').text(data.hello);
+      }
+    });
+  });
+});
+*/
